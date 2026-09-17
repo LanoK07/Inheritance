3d514a568b3edfca4873b628b6d1ffa8819fdfbe
Lanok07 <kellyd0207@students.bowiestate.edu>
2026-09-17 10:22:50 -0400

    inheritance-2

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+// Private and protected
+
 public class Person {
     // height, age, color, name, weight: instance variable
 
@@ -11,7 +13,9 @@ public class Person {
 
     double weight;
 
-    Person(){
+    // Cronstructors can have private, protected, public access modifiers
+
+   public Person(){
         // Default constructor can be empty
         height = 48;
         age = 18;
@@ -20,15 +24,28 @@ public class Person {
         weight = 140;
     }
 
+    // Protected members are accessible within the same package-Java files under same directory
+
+    protected Person(double height, int age){
+        //Parameters are local variables to the constructor
+        this.height= height;
+        this.age = age;
+    }
     // Overloaded constructor
 
-    Person(double height, int age, String color, String name, double weight){
+   private Person(double height, int age, String color, String name, double weight){
         //Parameters are local variables to the constructor
         this.height= height;
         this.age = age;
         this.color = color;
         this.name = name;
         this.weight = weight;
+
+    }
+
+    public Person Call_Private(){
+       Person p3 = new Person(72, 70, "Black", "Barack Obama", 170);
+       return p3;
 
     }
 }
